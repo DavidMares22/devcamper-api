4371341e7b1482f5559366b6489296b2aeeb2883
damr <damr18@gmail.com>
2021-02-13 09:31:08 -0600

    ErrorResponse class

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,3 +1,4 @@
+const ErrorResponse = require("../utils/errorResponse");
 const Bootcamp = require("../models/Bootcamp");
 
 // @desc Get all bootcamps
@@ -25,13 +26,21 @@ exports.getBootcamp = async (req, res, next) => {
     const bootcamp = await Bootcamp.findById(req.params.id);
 
     if (!bootcamp) {
-      return res.status(400).json({ success: false });
+      return next(
+        new ErrorResponse(
+          ` The bootcamp not forun with id of ${req.params.id}`,
+          404
+        )
+      );
     }
 
     res.status(200).json({ success: true, data: bootcamp });
   } catch (error) {
     // res.status(400).json({ success: false });
-    next(error);
+
+    next(
+      new ErrorResponse(`Bootcamp not forun with id of ${req.params.id}`, 404)
+    );
   }
 };
 
