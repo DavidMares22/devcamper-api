b0ebf6edbe0969be8db134e5025a900d4e426331
damr <damr18@gmail.com>
2021-02-13 09:12:06 -0600

    error handler middleware

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -8,7 +8,9 @@ exports.getBootcamps = async (req, res, next) => {
   try {
     const bootcamps = await Bootcamp.find();
 
-    res.status(200).json({ success: true, count: bootcamps.length,data: bootcamps });
+    res
+      .status(200)
+      .json({ success: true, count: bootcamps.length, data: bootcamps });
   } catch (error) {
     res.status(400).json({ success: false });
   }
@@ -28,7 +30,8 @@ exports.getBootcamp = async (req, res, next) => {
 
     res.status(200).json({ success: true, data: bootcamp });
   } catch (error) {
-    res.status(400).json({ success: false });
+    // res.status(400).json({ success: false });
+    next(error);
   }
 };
 
@@ -78,7 +81,6 @@ exports.deleteBootcamp = async (req, res, next) => {
     const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
 
     if (!bootcamp) {
-      
       return res.status(400).json({ success: false });
     }
     res.status(200).json({ success: true, data: {} });
