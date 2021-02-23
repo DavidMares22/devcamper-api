f01a8a5f958cf24f525386f2ae258c32c67e1047
damr <damr18@gmail.com>
2021-02-22 22:00:54 -0600

    get all courses or all from a specific bootcamp

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -5,12 +5,19 @@ const {
   deleteBootcamp,
   updateBootcamp,
   createBootcamp,
-  getBootcampsInRadius
+  getBootcampsInRadius,
 } = require("../controllers/bootcamps");
+
+// Include other resource routers
+const courseRouter = require("./courses");
+
 const router = express.Router();
 
+// Re-route into other resource routers
+router.use("/:bootcampId/courses", courseRouter);
+
 router.route("/").get(getBootcamps).post(createBootcamp);
-router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
+router.route("/radius/:zipcode/:distance").get(getBootcampsInRadius);
 
 router
   .route("/:id")
--- seeder.js
+++ seeder.js
@@ -8,6 +8,7 @@ dotenv.config({ path: './config/config.env' });
 
 // Load models
 const Bootcamp = require('./models/Bootcamp');
+const Course = require('./models/Course');
 
 // Connect to DB
 mongoose.connect(process.env.MONGO_URI, {
@@ -22,11 +23,15 @@ const bootcamps = JSON.parse(
     fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8')
   );
 
+const courses = JSON.parse(
+    fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8')
+  );
+
   // Import into DB
 const importData = async () => {
     try {
       await Bootcamp.create(bootcamps);
-    //   await Course.create(courses);
+      await Course.create(courses);
     //   await User.create(users);
     //   await Review.create(reviews);
       console.log('Data Imported...'.green.inverse);
@@ -40,7 +45,7 @@ const importData = async () => {
 const deleteData = async () => {
     try {
       await Bootcamp.deleteMany();
-    //   await Course.deleteMany();
+      await Course.deleteMany();
     //   await User.deleteMany();
     //   await Review.deleteMany();
       console.log('Data Destroyed...'.red.inverse);
