6c59733350df1b46e691d96fabc1222a5f214261
damr <damr18@gmail.com>
2021-02-27 18:28:50 -0600

    Login users

--- controllers/auth.js
+++ controllers/auth.js
@@ -18,52 +18,60 @@ exports.register = asyncHandler(async (req, res, next) => {
     role,
   });
 
-  res.status(200).json({ success: true });
+  // create token
+  const token = user.getSignedJwtToken()
+
+  res.status(200).json({ success: true, token });
   // sendTokenResponse(user, 200, res);
 });
 
 // @desc      Login user
 // @route     POST /api/v1/auth/login
 // @access    Public
-// exports.login = asyncHandler(async (req, res, next) => {
-//   const { email, password } = req.body;
+exports.login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
 
-//   // Validate emil & password
-//   if (!email || !password) {
-//     return next(new ErrorResponse("Please provide an email and password", 400));
-//   }
+  // Validate emil & password
+  if (!email || !password) {
+    return next(new ErrorResponse("Please provide an email and password", 400));
+  }
 
-//   // Check for user
-//   const user = await User.findOne({ email }).select("+password");
+  // Check for user
+  const user = await User.findOne({ email }).select("+password");
 
-//   if (!user) {
-//     return next(new ErrorResponse("Invalid credentials", 401));
-//   }
+  if (!user) {
+    return next(new ErrorResponse("Invalid credentials", 401));
+  }
 
-//   // Check if password matches
-//   const isMatch = await user.matchPassword(password);
+  // Check if password matches
+  const isMatch = await user.matchPassword(password);
 
-//   if (!isMatch) {
-//     return next(new ErrorResponse("Invalid credentials", 401));
-//   }
+  if (!isMatch) {
+    return next(new ErrorResponse("Invalid credentials", 401));
+  }
 
-//   sendTokenResponse(user, 200, res);
-// });
+  // create token
+  const token = user.getSignedJwtToken()
+
+  res.status(200).json({ success: true, token });
+
+  // sendTokenResponse(user, 200, res);
+});
 
 // @desc      Log user out / clear cookie
 // @route     GET /api/v1/auth/logout
 // @access    Private
-// exports.logout = asyncHandler(async (req, res, next) => {
-//   res.cookie("token", "none", {
-//     expires: new Date(Date.now() + 10 * 1000),
-//     httpOnly: true,
-//   });
+exports.logout = asyncHandler(async (req, res, next) => {
+  res.cookie("token", "none", {
+    expires: new Date(Date.now() + 10 * 1000),
+    httpOnly: true,
+  });
 
-//   res.status(200).json({
-//     success: true,
-//     data: {},
-//   });
-// });
+  res.status(200).json({
+    success: true,
+    data: {},
+  });
+});
 
 // @desc      Get current logged in user
 // @route     POST /api/v1/auth/me
--- models/User.js
+++ models/User.js
@@ -46,17 +46,17 @@ UserSchema.pre('save', async function(next) {
   this.password = await bcrypt.hash(this.password, salt);
 });
 
-// // Sign JWT and return
-// UserSchema.methods.getSignedJwtToken = function() {
-//   return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
-//     expiresIn: process.env.JWT_EXPIRE
-//   });
-// };
+// Sign JWT and return
+UserSchema.methods.getSignedJwtToken = function() {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE
+  });
+};
 
-// // Match user entered password to hashed password in database
-// UserSchema.methods.matchPassword = async function(enteredPassword) {
-//   return await bcrypt.compare(enteredPassword, this.password);
-// };
+// Match user entered password to hashed password in database
+UserSchema.methods.matchPassword = async function(enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
 // Generate and hash password token
 // UserSchema.methods.getResetPasswordToken = function() {
