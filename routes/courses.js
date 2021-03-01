b59e6b5f593c634f1c44f5721ccb4014ce7b7bf7
damr <damr18@gmail.com>
2021-02-28 21:10:26 -0600

    middleware for roles

--- routes/courses.js
+++ routes/courses.js
@@ -10,7 +10,7 @@ const {
 
 const router = express.Router({ mergeParams: true });
 
-const {protect} = require('../middleware/auth')
+const {protect, authorize} = require('../middleware/auth')
 
 const Course = require("../models/Course");
 const advancedResults = require('../middleware/advancedResults')
@@ -18,8 +18,8 @@ const advancedResults = require('../middleware/advancedResults')
 router.route("/").get(advancedResults(Course,{
   path: "bootcamp",
   select: "name description",
-}),getCourses).post(protect,addCourse);
+}),getCourses).post(protect, authorize('publisher','admin'),addCourse);
 
-router.route("/:id").get(getCourse).put(protect,updateCourse).delete(protect,deleteCourse);
+router.route("/:id").get(getCourse).put(protect, authorize('publisher','admin'),updateCourse).delete(protect, authorize('publisher','admin'),deleteCourse);
 
 module.exports = router;
