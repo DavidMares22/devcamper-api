4770f8db2e43360fd9a6914866948a4f20065e04
damr <damr18@gmail.com>
2021-03-04 10:11:04 -0600

    update email,name,pass

--- routes/auth.js
+++ routes/auth.js
@@ -18,8 +18,8 @@ router.post('/register', register);
 router.post('/login', login);
 // router.get('/logout', logout);
 router.get('/me', protect, getMe);
-// router.put('/updatedetails', protect, updateDetails);
-// router.put('/updatepassword', protect, updatePassword);
+router.put('/updatedetails', protect, updateDetails);
+router.put('/updatepassword', protect, updatePassword);
 router.post('/forgotpassword', forgotPassword);
 router.put('/resetpassword/:resettoken', resetPassword);
 
